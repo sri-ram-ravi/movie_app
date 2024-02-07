7b3bb13fb1a8fc2d6d2ce96147c77c1f9032fa0a
sri-ram-ravi <sriramravigit@gmail.com>
2024-02-07 22:07:51 +0530

    Implemented a request to movie db

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express=require("express")
 const app=express()
-app.listen(5000,()=>console.log("server is listening"))
+const request=require("request")
+
+app.listen(9900,()=>console.log("server is listening"))
 
 app.set("view engine","ejs")
 app.use(express.static('public'))
 
 app.get("/search_movie",(req,res)=> res.render("search"))
 
-app.get("/result",(req,res)=> res.render("result"))
+app.get("/result",(req,res)=>{
+
+    let query=req.query.search
+
+    // requesting to external api
+
+    const searchRequest=`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=0c300ff53789a02da04686e856581e41`
+
+    request(searchRequest,(err,body)=>{
+        if(err) console.log(err)
+        let data=JSON.parse(body)
+        res.render("result",{data:data,query:query})        
+    })
+
+})
